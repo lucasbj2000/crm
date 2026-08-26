4608a0a37e1dd4cef23a22049a93176e8765acf8
lucasbj2000 <lucmabeji2000@gmail.com>
2026-08-26 09:13:25 -0300

    chore: cargar navegación y responsive V25.3

--- app/public/v22.js
+++ app/public/v22.js
@@ -15,11 +15,14 @@
   addCss("/v24.css?v=24.0");
   addCss("/v24-1.css?v=24.1");
   addCss("/v25.css?v=25.2.1");
+  addCss("/v25-3.css?v=25.3");
   addScript("/v22-core.js?v=22-core", () =>
     addScript("/v24.js?v=24.0", () =>
       addScript("/v24-1.js?v=24.1", () =>
         addScript("/v25-2-1.js?v=25.2.1", () =>
-          addScript("/v25.js?v=25.2.1")
+          addScript("/v25-3.js?v=25.3", () =>
+            addScript("/v25.js?v=25.2.1")
+          )
         )
       )
     )
