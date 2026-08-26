2e54a51da880e37ba9fc17315cd262a16d5b5dd0
lucasbj2000 <lucmabeji2000@gmail.com>
2026-08-26 10:11:41 -0300

    chore: renovar loader V25.4.2

--- app/public/v22.js
+++ app/public/v22.js
@@ -17,6 +17,7 @@
   addCss("/v25.css?v=25.2.1");
   addCss("/v25-3.css?v=25.3");
   addCss("/v25-4.css?v=25.4.1");
+  addCss("/v25-4-2.css?v=25.4.2");
   addScript("/v22-core.js?v=22-core", () =>
     addScript("/v24.js?v=24.0", () =>
       addScript("/v24-1.js?v=24.1", () =>
@@ -24,7 +25,7 @@
           addScript("/v25-3.js?v=25.3", () =>
             addScript("/v25.js?v=25.2.1", () =>
               addScript("/v25-4.js?v=25.4", () =>
-                addScript("/v25-4-1.js?v=25.4.1")
+                addScript("/v25-4-1.js?v=25.4.2")
               )
             )
           )
